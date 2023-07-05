fd3b98067f517e9b317554d2a56c45a271b83e9f
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-07-05 11:40:56 +0200

    Récupérer les statistique/historique des envois en JAVA

---
+++ src/StatistiqueDesEnvois.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class StatistiqueDesEnvois {
+    public static void main(String[] args) {
+        try {
+            // Prepare data for GET request
+            String apiKey = "YOUR_API_KEY";
+            String interval = "last_twelve_months";
+            //interval=last_month // 1 dernier mois
+//interval=custom&from=21-10-2022&to=21-10-2022 // intervalle personnalisé
+
+            // Create GET request URL
+            String urlString = "https://api.smspartner.fr/v1/statistics/cost-resume?" +
+                    "apiKey=" + apiKey + "&interval=" + interval;
+
+            // Create URL object
+            URL url = new URL(urlString);
+
+            // Create HTTP connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(10000);
+
+            // Send GET request
+            int responseCode = connection.getResponseCode();
+
+            // Get response
+            BufferedReader reader;
+            if (responseCode >= 200 && responseCode <= 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Process your response here
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
